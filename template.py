241bbad77e2939b1ad990218e0295771c3fb5429
ezjanos <oskar.jansson@ericsson.com>
2024-12-12 11:47:17 +0100

    adds template.py

---
+++ template.py
@@ -0,0 +1,20 @@
+example1 = """"""
+example2 = """"""
+puzzle = """"""
+
+from color import Color
+
+def solve1(p) -> int:
+    pass
+
+def solve2(p) -> int:
+    pass
+
+if __name__ == '__main__':
+    print(solve1(example1))
+    # print(solve1(example2))
+    # print(solve1(puzzle))
+    #
+    # print(solve2(example1))
+    # print(solve2(example2))
+    # print(solve2(puzzle))
